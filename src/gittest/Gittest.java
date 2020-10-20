e54f9ad6f542729fa523a8f4f808cbf1359b079c
Yuthasart <yuthasart.oat@mail.kmutt.ac.th>
2020-10-20 15:13:15 +0700

    1

---
+++ src/gittest/Gittest.java
@@ -0,0 +1,14 @@
+
+package gittest;
+
+
+public class Gittest {
+
+    
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+}
